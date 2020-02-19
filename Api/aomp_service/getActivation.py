cfaef581c687435db3347766795ee46edd73d7fc
xiaoxiaoer <1157123982@qq.com>
2020-02-19 17:13:00 +0800

    新增车场流程

--- Api/aomp_service/getActivation.py
+++ Api/aomp_service/getActivation.py
@@ -3,43 +3,22 @@
  微信公众号：泉头活水
 """
 import json
-import time
-import requests
 
-from Api.aomp_service.pompLogin import pomp, Operation_parking
-from Api.parking_service.createParking_service import CreateParking
-from common.db import Db as db
+from Api.Login import AompLogin
+from common.Req import Req
+from common.db import Db
 from common.superAction import SuperAction
 
+form_headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
-class aomp(object):
-    cooperativeAccount = 'test'+SuperAction().get_time()
 
-    """校验验证码"""
-    def __init__(self, host="http://aomp-grey.k8s.yidianting.com.cn"):
-        self.host = host
-        loginUrl = self.host + "/checkLogin.do"
-        self.S = requests.session()
-        data = {
-            "user": "lgc",
-            "pwd": "999999",
-            "validateCode": "9999",
-            "isOnLine": "isOnLine",
-            "flag": "-1"
-        }
-        self.S.post(loginUrl, data)
+class ActivationInfo(Req):
 
-    def login(self):
-        """登陆aomp"""
-        path = self.host + "/admin/loginTomanage.do?flag=admin&flag=admin"
-        data = {
-            "flag": "admin"
-        }
-        self.S.post(path, data)
+    # cooperativeAccount = 'test'+SuperAction().get_time()
 
     def add_cooperative_user(self, cooperativeAccount):
-        """添加授权商"""
-        path = self.host + "/cooperativeUser/saveCooperativeUser.do"
+        """添加合作商"""
+        self.url = "/cooperativeUser/saveCooperativeUser.do"
         data = {
             "cooperativeName": "test合作商{}".format(SuperAction().get_time()),
             "cooperativeAccount": "{}".format(cooperativeAccount),
@@ -47,74 +26,42 @@ class aomp(object):
             "isOkPassword": "999999",
             "weixinAccountId": "2"
         }
-        self.S.post(path, data)
 
-    def add_activation(self, cooperativeUserId):
-        """新增并生成激活码"""
+        self.post(self.aomp_api, data=data, headers=form_headers)
 
-        path_add = self.host + "/cooperativeCode/addCooperativeActivation.do"
-        data_add = {
+    def get_activation(self, cooperativeAccount):
+
+        """新增激活码"""
+        sql = "SELECT id FROM cooperative_user WHERE cooperative_account = '{}' ORDER BY id DESC".format(cooperativeAccount)
+        cooperativeUserId = Db().select(sql)
+        self.url = "/cooperativeCode/addCooperativeActivation.do"
+        data = {
             "cooperativeUserId": "{}".format(cooperativeUserId),
             "codeTotal": "10"
         }
-        self.S.post(path_add, data_add)
+        self.post(url=self.aomp_api, data=data, headers=form_headers)
 
-    def pro_activation(self, cooperativeUserId, activationId):
-        path_pro = self.host + "/cooperativeCode/produceCode.do"
-        data_pro = {
+        """生成激活码"""
+        sql = "SELECT id FROM cooperative_activation WHERE cooperativeUser_id = '{}'".format(cooperativeUserId)
+        activationId = Db().select(sql)
+        self.url = "/cooperativeCode/produceCode.do"
+        data = {
             "cooperativeUserId": "{}".format(cooperativeUserId),
             "activationId": "{}".format(activationId)
         }
-        self.S.post(path_pro, data_pro)
+        self.post(url=self.aomp_api, data=data, headers=form_headers)
 
-    def getCodeDetail(self, activationId):
-        """新增并生成激活码、获取激活码"""
-        path = self.host + "/cooperativeCode/getCodeDetail.do"
+        """获取激活码"""
+        self.url = "/cooperativeCode/getCodeDetail.do"
         data = {
             "page": 1,
             "rows": "10",
             "activationId": "{}".format(activationId)
         }
-        r = self.S.post(path, data)
-        print(r.text)
-        print(type(r.text))
-        s = json.loads(r.text)
-        return s['rows'][0]['activationCode']
-
-    def main(self):
-        """登陆aomp-新增合作商-新增激活码-生成激活码-获取激活码"""
-        self.login()
-        self.add_cooperative_user(self.cooperativeAccount)
-        cooperativeUserIdSql = "SELECT id FROM cooperative_user WHERE cooperative_account ='" + self.cooperativeAccount + "'"
-        cooperativeUserId = db().select(cooperativeUserIdSql)
-        self.add_activation(cooperativeUserId)
-        time.sleep(1)
-        activationIdSql = "SELECT id FROM cooperative_activation WHERE cooperativeUser_id ={}".format(cooperativeUserId)
-        activationId = db().select(activationIdSql)
-        self.pro_activation(cooperativeUserId, activationId)
-        time.sleep(1)
-        activationCode = self.getCodeDetail(activationId)
-
-        '''检查激活码-获取pomp验证码-校验pomp验证码-登陆pomp-登陆检查'''
-
-
-        '''新增停车场'''
-
-        return activationId, activationCode
+        r = self.post(url=self.aomp_api, data=data, headers=form_headers).json()
+        return r['rows'][0]['activationCode']
 
 
 if __name__ == '__main__':
-    userAccount = "test{}".format(SuperAction().get_time())
-    a = aomp()
-    activationId, activationCode = a.main()
-    p = Operation_parking()
-    p.validateActivationCode(activationCode)
-    p.validUser(userAccount)
-    print("userAccount", userAccount)
-    p.registerUser(activationCode, userAccount)
-    p = pomp(userAccount)
-    time.sleep(1)
-    c = CreateParking(p.userAccount).newParking(activationCode)
-    print(c.text)
-
+    a = AompLogin()
 
--- test_suite/parking_service/parkConfig_controller/test_parkConfig.py
+++ test_suite/parking_service/parkConfig_controller/test_parkConfig.py
@@ -2,3 +2,44 @@
  Created by lgc on 2020/2/19 15:37.
  微信公众号：泉头活水
 """
+
+import os
+
+import allure
+import pytest
+
+from Api.aomp_service.getActivation import ActivationInfo
+from Api.parking_service.createParking_service import CreateParking, Operation_parking
+from conftest import aompLogin
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+root_path = os.path.abspath(os.path.join(BASE_DIR, "../.."))
+from common.utils import YmlUtils
+
+
+args_item = "send_data,expect"
+test_data, case_desc = YmlUtils("/test_data/parking_service/parkConfig_controller/createParking.yml").getData
+@pytest.mark.parametrize(args_item, test_data)
+@allure.feature("创建停车场")
+
+class Test_createParking():
+
+    """添加授权商"""
+    def test_add_cooperative_use(self,aompLogin, send_data, expect):
+        ActivationInfo(aompLogin).add_cooperative_user(send_data['cooperativeAccount'])
+
+    """获取激活码"""
+    def test_get_activation(self, aompLogin, send_data, expect):
+        ActivationInfo(aompLogin).get_activation(send_data['cooperativeAccount'])
+
+    """注册运营商"""
+    def test_createPomp(self, send_data, expect):
+
+        Operation_parking().validateActivationCode(send_data['cooperativeAccount'])
+        Operation_parking().validAndregister_User(send_data['cooperativeAccount'], send_data['userAccount'])
+
+    """新增停车场"""
+    def test_createParking(self, userLogin, send_data, expect):
+        CreateParking(userLogin).newParking(send_data['cooperativeAccount'])
+        # CreateParking(userLogin).newParking("test20200219165316")
+
