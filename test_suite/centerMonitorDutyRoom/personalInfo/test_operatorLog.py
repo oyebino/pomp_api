4bb591cc6947ec64da6b85d8ef014cd7f82d9c71
xiaoxiaoer <1157123982@qq.com>
2020-03-11 15:37:21 +0800

    中央值守日志操作

--- test_suite/centerMonitorDutyRoom/personalInfo/test_operatorLog.py
+++ test_suite/centerMonitorDutyRoom/personalInfo/test_operatorLog.py
@@ -20,20 +20,20 @@ test_data, case_desc = YmlUtils("/test_data/centerMonitorRoom/personalInfo/opera
 @allure.feature("中央值守操作日志")
 class TestOperatorLog(BaseCase):
 
-    def test_mockCarIn(self, send_data, expect):
-        """模拟进场"""
-        re = cloudparking_service().mockCarInOut(send_data["carNum"], 0, send_data["StrictRule_inClientID"])
-        result = re.json()
-        self.save_data('carIn_jobId', result['biz_content']['job_id'])
-        Assertions().assert_in_text(result, expect["mockCarInMessage"])
-
-    def test_checkIn(self, sentryLogin, send_data, expect):
-        """登记放行-此处应为中央值守的登记放行-未完成"""
-        re = CarInOutHandle(sentryLogin).carInOutHandle(send_data['carNum'], send_data['carInHandleType'],send_data['carIn_jobId'])
-        result = re.json()['biz_content']['result']
-        Assertions().assert_in_text(result['screen'], expect["checkCarInScreen"])
-        Assertions().assert_in_text(result['voice'], expect["checkCarInVoice"])
-        Assertions().assert_in_text(result['open_gate'], expect["checkCarInOpenGate"])
+    # def test_mockCarIn(self, send_data, expect):
+    #     """模拟进场"""
+    #     re = cloudparking_service().mockCarInOut(send_data["carNum"], 0, send_data["StrictRule_inClientID"])
+    #     result = re.json()
+    #     self.save_data('carIn_jobId', result['biz_content']['job_id'])
+    #     Assertions().assert_in_text(result, expect["mockCarInMessage"])
+    #
+    # def test_checkIn(self, sentryLogin, send_data, expect):
+    #     """登记放行-此处应为中央值守的登记放行-未完成"""
+    #     re = CarInOutHandle(sentryLogin).carInOutHandle(send_data['carNum'], send_data['carInHandleType'],send_data['carIn_jobId'])
+    #     result = re.json()['biz_content']['result']
+    #     Assertions().assert_in_text(result['screen'], expect["checkCarInScreen"])
+    #     Assertions().assert_in_text(result['voice'], expect["checkCarInVoice"])
+    #     Assertions().assert_in_text(result['open_gate'], expect["checkCarInOpenGate"])
 
 
     def test_operatorLog(self,centerMonitorLogin, send_data, expect):
