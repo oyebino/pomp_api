50546e2bdf42504a766f75405e46a0e127b3c62d
xiaoxiaoer <1157123982@qq.com>
2020-03-12 12:18:00 +0800

    收费员新增-修改

--- Api/parkingManage_service/tollCollection.py
+++ Api/parkingManage_service/tollCollection.py
@@ -2,3 +2,66 @@
  Created by lgc on 2020/3/12 9:18.
  微信公众号：泉头活水
 """
+from common.Req import Req
+from common.superAction import SuperAction
+
+
+class TollCollection(Req):
+    """
+    收费员账号
+    """
+    api_headers = {"Content-Type": "application/json;charset=UTF-8"}
+    nickName = "test" + SuperAction().get_time()
+
+    def add_tollCollection(self, userId, pwd):
+        """
+        新增收费员-需绑定车场！！！
+        """
+        self.url = "/mgr/user/sentryuser/add"
+        json_data = {
+                        "nickName": "{}".format(self.nickName),
+                        "role": "0",
+                        "telephone": "18000000000",
+                        "id": None,
+                        "userId": "{}".format(userId),
+                        "password": "{}".format(pwd)
+                  }
+        re = self.post(self.api, json=json_data, headers=self.api_headers)
+        return re
+
+    def modify_tollCollection(self, editUserId, editPwd):
+        """
+        修改收费员
+        """
+        allNameDict = self.getDictBykey(self.__queryAllTollCollection().json(),'nickName',self.nickName)
+        print("self.nickName****",self.nickName)
+        print("allNameDict****",allNameDict)
+        id = allNameDict['id']
+        print(id)
+        self.url = "/mgr/user/sentryuser/update"
+        json_data = {
+                        "nickName": "test{}".format(SuperAction().get_time()),
+                        "role": "0",
+                        "telephone": "18000000000",
+                        "id": "{}".format(id),
+                        "userId": "{}".format(editUserId),
+                        "password": "{}".format(editPwd)
+                  }
+        re = self.post(self.api, json=json_data, headers=self.api_headers)
+        print("modify_tollCollection&&***",re.text)
+        return re
+
+    def __queryAllTollCollection(self):
+        self.url = "/mgr/user/sentryuser/getAll"
+        json_data = {
+                        "pageNumber": 1,
+                        "pageSize": 250,
+                        "sortType": "ASC"  # 取最新的，需排序
+                    }
+        re = self.post(self.api, json=json_data, headers=self.api_headers)
+        print("__queryAllTollCollection**",re.text)
+        return re
+
+if __name__ == '__main__':
+    re = TollCollection().add_tollCollection("test006")
+    print(re.text)
--- test_suite/parkingManage/tollCollection/test_modifyTollCollection.py
+++ test_suite/parkingManage/tollCollection/test_modifyTollCollection.py
@@ -1,5 +1,5 @@
 """
- Created by lgc on 2020/3/12 9:28.
+ Created by lgc on 2020/3/12 10:28.
  微信公众号：泉头活水
 """
 
@@ -12,24 +12,42 @@ from common.utils import YmlUtils
 from common.Assert import Assertions
 
 args_item = "send_data,expect"
-test_data, case_desc = YmlUtils("/test_data/parkingManage/tollCollection/addToll.yml").getData
+test_data, case_desc = YmlUtils("/test_data/parkingManage/tollCollection/modifyToll.yml").getData
 
 
 @pytest.mark.parametrize(args_item, test_data)
 @allure.feature("收费员账号")
-class TestAddTollection(BaseCase):
+class TestModifyTollection(BaseCase):
 
     """增加收费员"""
-    def test_addToll(self,userLogin, send_data, expect):
+
+    def test_addToll(self, userLogin, send_data, expect):
         re = TollCollection(userLogin).add_tollCollection(send_data['userId'], send_data['pwd'])
         self.save_data('userId', send_data['userId'])
         self.save_data('pwd', send_data['pwd'])
-        status = re.json()['status']
-        Assertions.assert_in_text(status, expect['status'])
+        status = re.json()
+        Assertions().assert_in_text(status, expect['status'])
 
-    """收费员登陆中央收费页面"""
+    """修改收费员"""
+    def test_modifyToll(self,userLogin, send_data, expect):
+        re = TollCollection(userLogin).modify_tollCollection(send_data['editUserId'], send_data['editPwd'])
+        self.save_data('userId', send_data['userId'])
+        self.save_data('pwd', send_data['pwd'])
+        self.save_data('editUserId', send_data['editUserId'])
+        self.save_data('editPwd', send_data['editPwd'])
+        status = re.json()
+        Assertions().assert_in_text(status, expect['status'])
+
+    """使用之前的用户-登陆中央收费"""
     @pytest.mark.parametrize('centralTollLogin', [{'user': '${mytest.userId}', 'pwd': '${mytest.pwd}'}], indirect=True)
     def test_loginCentral(self, centralTollLogin, send_data, expect):
         re = CentralPersonalInfo(centralTollLogin).duty_info()
-        onDutyTime = re.json()['on_duty_time']
-        Assertions.assert_in_text(onDutyTime, expect['on_duty_time'])
+        onDutyTime = re.json()
+        Assertions().assert_in_text(onDutyTime, expect['onDutyTime'])
+
+    """使用修改后的用户-登陆中央收费"""
+    @pytest.mark.parametrize('centralTollLogin', [{'user': '${mytest.editUserId}', 'pwd': '${mytest.editPwd}'}], indirect=True)
+    def test_modifyLoginCentral(self, centralTollLogin, send_data, expect):
+        re = CentralPersonalInfo(centralTollLogin).duty_info()
+        onDutyTime = re.json()
+        Assertions().assert_in_text(onDutyTime, expect['onDutyTime'])
